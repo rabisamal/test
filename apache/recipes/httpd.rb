2a6a8841b42ec7f78eea2f7f1d147fed6f879127
root <root@ip-172-31-4-193.us-west-2.compute.internal>
2014-08-07 11:50:26 +0000

    modified:   httpd.rb 	new file:   ../templates/default/index.html.erb

--- apache/recipes/httpd.rb
+++ apache/recipes/httpd.rb
@@ -1,7 +1,12 @@
 package "httpd" do
 action :install
 end
-
+template "/var/www/html/index.html" do
+  source "index.html.erb"
+  owner "root"
+  group "root"
+  mode "644"
+end
 # Stop iptables service permanently
 service "iptables" do
         action [:disable,:stop]
